2850103049ed40d5d9a1a3c64ceb060f6b052203
Jeffry Troll <jeffrytroll98@gmail.com>
2023-12-14 10:40:12 -0700

    Update PROJECT_YOUTUBE_DASHBOARD.py

--- PROJECT_YOUTUBE_DASHBOARD.py
+++ PROJECT_YOUTUBE_DASHBOARD.py
@@ -65,12 +65,12 @@ filtered_df = df[(df['channel_name'] == selected_channel) & (df['year'] == selec
 videos_df = filtered_df[filtered_df['short'] == 0]
 shorts_df = filtered_df[filtered_df['short'] == 1]
 
-# Create two line charts, one for videos and one for shorts
-fig_videos = px.line(videos_df, x='published', y=selected_metric,
+# Create two line charts, one for videos and one for shorts, with data point labels
+fig_videos = px.line(videos_df, x='published', y=selected_metric, text=videos_df['title'],
                      title=f'Line Chart of {metric_names[selected_metric]} for Videos',
                      labels={'published': 'Published Date', selected_metric: metric_names[selected_metric]})
 
-fig_shorts = px.line(shorts_df, x='published', y=selected_metric,
+fig_shorts = px.line(shorts_df, x='published', y=selected_metric, text=shorts_df['title'],
                      title=f'Line Chart of {metric_names[selected_metric]} for Shorts',
                      labels={'published': 'Published Date', selected_metric: metric_names[selected_metric]})
 
